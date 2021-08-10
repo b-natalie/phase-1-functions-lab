24bbd658126d475606b93972d34b68106fecae21
Natalie Barba <nbarba2010@gmail.com>
2021-08-09 20:52:31 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return (Math.abs(start - end) * 264);
+}
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start, end);
+    let fare;
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance <= 2000) {
+        fare = (distance - 400) * .02;
+    } else if (distance <= 2500) {
+        fare = 25;
+    } else {
+        fare = 'cannot travel that far';
+    }
+    return fare;
+}
